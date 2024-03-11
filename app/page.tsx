4a8ec46df8b2e7b10caa048199a931641f9d7c48
thegrtnx <thegrtnx@duck.com>
2024-03-12 00:06:07 +0100

    👌Update: Manifest

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
 	return (
 		<main className="h-screen">
 			<iframe
-				src="https://ballowallet.com/mobile/login/"
+				src={process.env.NEXT_PUBLIC_URL}
 				className="w-screen h-screen"></iframe>
 		</main>
 	);
